49ac9ce0b73f262ecd56b9b50b9f4f709a087fac
VladStroi <vladislavstroy@gmail.com>
2023-09-13 17:23:52 +0300

    ADD: Connect wallet

--- staking-daap/src/App.jsx
+++ staking-daap/src/App.jsx
@@ -1,18 +1,30 @@
-import './App.css'
+import "./App.css";
 
-import { Footer } from './components/header-footer/footer/footer'
-import { Header } from './components/header-footer/header/header'
-import { MainBar } from './components/main bar/main-bar'
-import { StakeMenu } from './components/stake manu/stake-menu'
+import { Footer } from "./components/header-footer/footer/footer";
+import { Header } from "./components/header-footer/header/header";
+import { MainBar } from "./components/main bar/main-bar";
+import { StakeMenu } from "./components/stake manu/stake-menu";
+
+import { WagmiConfig, createConfig, mainnet } from "wagmi";
+import { createPublicClient, http } from "viem";
+
+const config = createConfig({
+  autoConnect: true,
+  publicClient: createPublicClient({
+    chain: mainnet,
+    transport: http(),
+  }),
+});
 
 export const App = () => {
   return (
     <>
-      <Header/>
-      <MainBar/>
-      <StakeMenu/>
-      <Footer/>
+      <WagmiConfig config={config}>
+      <Header />
+      <MainBar />
+      <StakeMenu />
+      <Footer />
+      </WagmiConfig>
     </>
-  )
-}
-
+  );
+};
--- staking-daap/src/components/header-footer/header/header.jsx
+++ staking-daap/src/components/header-footer/header/header.jsx
@@ -2,13 +2,40 @@ import { StatusBar } from "../status-bar/status-bar";
 import styles from "./header.module.css";
 import logo from "./logo.png";
 
+import { useAccount, useBalance, useConnect, useToken   } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+
 export const Header = () => {
-  const scrollToForm = () => {
-    window.scrollTo({
-      top: document.body.scrollHeight,
-      behavior: "smooth",
-    });
-  };
+  const { address, isConnected } = useAccount();
+  const { connect } = useConnect({
+    connector: new InjectedConnector(),
+  });
+  const { data, isError, isLoading } = useBalance({
+    address: "0xA0Cf798816D4b9b9866b5330EEa46a18382f251e",
+  });
+  const { token, isError2, isLoading2 } = useToken({
+    address: '0xc18360217d8f7ab5e7c516566761ea12ce7f9d72',
+  })
+  if (isConnected)
+    return (
+      <>
+        <StatusBar />
+        <header className={styles.header}>
+          <div>
+            <a href="https://dexola.com/" target="_blank">
+              <img src={logo} alt="logo" />
+            </a>
+          </div>
+          <div>
+            <div>
+              {data?.symbol} {data?.formatted.substring(0, 6)} 
+            </div>
+            {/* btn or address with address*/}
+            <span>{address.substring(0, 16)}...</span>
+          </div>
+        </header>
+      </>
+    );
 
   return (
     <>
@@ -21,7 +48,7 @@ export const Header = () => {
         </div>
         <div>
           {/* btn or address with address*/}
-          <button>Connect wallet</button>
+          <button onClick={() => connect()}>Connect Wallet</button>
         </div>
       </header>
     </>
