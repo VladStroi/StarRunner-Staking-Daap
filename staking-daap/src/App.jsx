007c2f9d58ca019a06ca9475efd9f74b7a9ac3e3
VladStroi <vladislavstroy@gmail.com>
2023-09-18 17:20:15 +0300

    UPD: createConfig

--- staking-daap/src/App.jsx
+++ staking-daap/src/App.jsx
@@ -5,26 +5,13 @@ import { Header } from "./components/header-footer/header/header";
 import { MainBar } from "./components/main bar/main-bar";
 import { StakeMenu } from "./components/stake manu/stake-menu";
 
-import { WagmiConfig, createConfig, sepolia } from "wagmi";
-import { createPublicClient, http } from "viem";
-
-const config = createConfig({
-  autoConnect: true,
-  publicClient: createPublicClient({
-    chain: sepolia,
-    transport: http(),
-  }),
-});
-
 export const App = () => {
   return (
     <>
-      <WagmiConfig config={config}>
         <Header />
         <MainBar />
         <StakeMenu />
         <Footer />
-      </WagmiConfig>
     </>
   );
 };
--- staking-daap/src/main.jsx
+++ staking-daap/src/main.jsx
@@ -3,11 +3,46 @@ import ReactDOM from "react-dom/client";
 import { App } from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import {
+  WagmiConfig,
+  configureChains,
+  createConfig,
+  mainnet,
+  sepolia,
+} from "wagmi";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+// import { createPublicClient, http } from "viem";
+
+const { chains, publicClient } = configureChains(
+  [sepolia],
+  [
+    infuraProvider({ apiKey: "b0f4f2bdbc524cf3b57cd014475e319d" }),
+    publicProvider(),
+  ]
+);
+
+export const config = createConfig({
+  autoConnect: true,
+  connectors: [new MetaMaskConnector({ chains })],
+  publicClient,
+});
+
+// const config = createConfig({
+//   autoConnect: true,
+//   publicClient: createPublicClient({
+//     chain: sepolia,
+//     transport: http(),
+//   }),
+// });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <WagmiConfig config={config}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </WagmiConfig>
   </React.StrictMode>
 );
