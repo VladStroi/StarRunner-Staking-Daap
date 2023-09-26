0c405000851b6f9ecd1c47e98dd2c5d491682f32
VladStroi <vladislavstroy@gmail.com>
2023-09-26 20:16:03 +0300

    TEST: mobile connect wallet 3

--- staking-daap/src/components/stake manu/not-connect-wallet/not-connect.jsx
+++ staking-daap/src/components/stake manu/not-connect-wallet/not-connect.jsx
@@ -4,7 +4,7 @@ import { WalletIcon } from "./wallet-icon";
 
 import { useWeb3Modal } from "@web3modal/react";
 
-import { sepolia, useConnect } from "wagmi";
+import { sepolia, useConnect, useAccount } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
@@ -12,29 +12,30 @@ import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 export const NotConnect = () => {
-  const { connect } = useConnect({
-    connector:
-      // new MetaMaskConnector({ chains: [sepolia] }),
-      // new CoinbaseWalletConnector({
-      //           chain: sepolia,
+  const { isConnected } = useAccount();
+  // const { connect } = useConnect({
+  //   connector:
+  //     new MetaMaskConnector({ chains: [sepolia] }),
+  //     new CoinbaseWalletConnector({
+  //               chain: sepolia,
 
-      //   options: {
-      //     appName: "wagmi",
-      //   },
-      // }),
-      // new WalletConnectConnector({
-      //   options: {
-      //     projectId: "d1ed4a8b1d08ee63e62097023077970f",
-      //   },
-      // }),
-    new InjectedConnector({
-      chain: sepolia,
-      options: {
-        name: "Injected",
-        shimDisconnect: true,
-      },
-    }),
-  });
+  //       options: {
+  //         appName: "wagmi",
+  //       },
+  //     }),
+  //     new WalletConnectConnector({
+  //       options: {
+  //         projectId: "d1ed4a8b1d08ee63e62097023077970f",
+  //       },
+  //     }),
+  //   new InjectedConnector({
+  //     chain: sepolia,
+  //     options: {
+  //       name: "Injected",
+  //       shimDisconnect: true,
+  //     },
+  //   }),
+  // });
 
   return (
     <section className={styles.sectionWindow}>
@@ -43,7 +44,18 @@ export const NotConnect = () => {
         <CircleError />
       </div>
       <span>To start staking you need to connect you wallet first</span>
-      <ConnectButton />
+      <ConnectButton.Custom>
+      {({ openConnectModal, connectModalOpen }) => {
+        return (
+          <button
+            disabled={isConnected && connectModalOpen}
+            onClick={openConnectModal}
+          >
+            Connect Wallet
+          </button>
+        );
+      }}
+    </ConnectButton.Custom>
       {/* <button onClick={() => connect()}>Connect Wallet</button> */}
     </section>
   );
