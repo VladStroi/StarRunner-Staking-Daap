912fa9bc4afff3c954b06df2e1fd7a5bfb1788e2
VladStroi <vladislavstroy@gmail.com>
2023-09-26 17:11:00 +0300

    TEST: mobile connect wallet (rainbowkit)

--- staking-daap/src/App.jsx
+++ staking-daap/src/App.jsx
@@ -1,42 +1,69 @@
 import "./App.css";
 
-import {
-  EthereumClient,
-  w3mConnectors,
-  w3mProvider,
-} from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
-import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { sepolia, mainnet } from "wagmi/chains";
-  import { infuraProvider } from "wagmi/providers/infura";
+// import {
+//   EthereumClient,
+//   w3mConnectors,
+//   w3mProvider,
+// } from "@web3modal/ethereum";
+// import { Web3Modal } from "@web3modal/react";
+// import { configureChains, createConfig, WagmiConfig } from "wagmi";
+// import { sepolia, mainnet } from "wagmi/chains";
+import "@rainbow-me/rainbowkit/styles.css";
 
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { mainnet, sepolia } from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
 import { Footer } from "./components/header-footer/footer/footer";
 import { Header } from "./components/header-footer/header/header";
 import { MainBar } from "./components/main bar/main-bar";
 import { StakeMenu } from "./components/stake manu/stake-menu";
 
-const chains = [sepolia, mainnet];
-const projectId = "d1ed4a8b1d08ee63e62097023077970f";
-const provaiderApiKey = "b0f4f2bdbc524cf3b57cd014475e319d"
+// const chains = [sepolia, mainnet];
+// const projectId = "d1ed4a8b1d08ee63e62097023077970f";
+// const provaiderApiKey = "b0f4f2bdbc524cf3b57cd014475e319d"
 
-const { publicClient } = configureChains(chains, [ infuraProvider({ apiKey: provaiderApiKey })]);
+// const { publicClient } = configureChains(chains, [ infuraProvider({ apiKey: provaiderApiKey })]);
+// const wagmiConfig = createConfig({
+//   autoConnect: true,
+//   connectors: w3mConnectors({ projectId, chains }),
+//   publicClient,
+// });
+// const ethereumClient = new EthereumClient(wagmiConfig, chains);
+const projectId = "d1ed4a8b1d08ee63e62097023077970f";
+const provaiderApiKey = "b0f4f2bdbc524cf3b57cd014475e319d";
+const { chains, publicClient } = configureChains(
+  [sepolia, mainnet],
+  [alchemyProvider({ apiKey: provaiderApiKey }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "connect",
+  projectId,
+  chains,
+});
+
 const wagmiConfig = createConfig({
   autoConnect: true,
-  connectors: w3mConnectors({ projectId, chains }),
+  connectors,
   publicClient,
 });
-const ethereumClient = new EthereumClient(wagmiConfig, chains);
-
 export const App = () => {
   return (
     <>
+      {/* <WagmiConfig config={wagmiConfig}> */}
       <WagmiConfig config={wagmiConfig}>
-        <Header />
-        <MainBar />
-        <StakeMenu />
-        <Footer />
+        <RainbowKitProvider chains={chains}>
+          <Header />
+          <MainBar />
+          <StakeMenu />
+          <Footer />
+        </RainbowKitProvider>
       </WagmiConfig>
-      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} />
+      {/* </WagmiConfig>
+      <Web3Modal projectId={projectId} ethereumClient={ethereumClient} /> */}
     </>
   );
 };
--- staking-daap/src/components/stake manu/not-connect-wallet/not-connect.jsx
+++ staking-daap/src/components/stake manu/not-connect-wallet/not-connect.jsx
@@ -2,15 +2,39 @@ import styles from "./not-connect.module.css";
 import { CircleError } from "./circle-error";
 import { WalletIcon } from "./wallet-icon";
 
-import { useWeb3Modal } from '@web3modal/react'
+import { useWeb3Modal } from "@web3modal/react";
 
-import { useConnect } from "wagmi";
+import { sepolia, useConnect } from "wagmi";
 import { InjectedConnector } from "wagmi/connectors/injected";
+import { MetaMaskConnector } from "wagmi/connectors/metaMask";
+import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { ConnectButton } from '@rainbow-me/rainbowkit'
 
 export const NotConnect = () => {
-  const { open } = useWeb3Modal({
-      connector: new InjectedConnector(),
-    })
+  const { connect } = useConnect({
+    connector:
+      // new MetaMaskConnector({ chains: [sepolia] }),
+      // new CoinbaseWalletConnector({
+      //           chain: sepolia,
+
+      //   options: {
+      //     appName: "wagmi",
+      //   },
+      // }),
+      // new WalletConnectConnector({
+      //   options: {
+      //     projectId: "d1ed4a8b1d08ee63e62097023077970f",
+      //   },
+      // }),
+    new InjectedConnector({
+      chain: sepolia,
+      options: {
+        name: "Injected",
+        shimDisconnect: true,
+      },
+    }),
+  });
 
   return (
     <section className={styles.sectionWindow}>
@@ -19,7 +43,104 @@ export const NotConnect = () => {
         <CircleError />
       </div>
       <span>To start staking you need to connect you wallet first</span>
-          <button onClick={() => open()}>Connect Wallet</button>
+      <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== 'loading';
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus ||
+            authenticationStatus === 'authenticated');
+
+        return (
+          <div
+            {...(!ready && {
+              'aria-hidden': true,
+              'style': {
+                opacity: 0,
+                pointerEvents: 'none',
+                userSelect: 'none',
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <button onClick={openConnectModal} type="button">
+                    Connect Wallet
+                  </button>
+                );
+              }
+
+              if (chain.unsupported) {
+                return (
+                  <button onClick={openChainModal} type="button">
+                    Wrong network
+                  </button>
+                );
+              }
+
+              return (
+                <div style={{ display: 'flex', gap: 12 }}>
+                  <button
+                    onClick={openChainModal}
+                    style={{ display: 'flex', alignItems: 'center' }}
+                    type="button"
+                  >
+                    {chain.hasIcon && (
+                      <div
+                        style={{
+                          background: chain.iconBackground,
+                          width: 12,
+                          height: 12,
+                          borderRadius: 999,
+                          overflow: 'hidden',
+                          marginRight: 4,
+                        }}
+                      >
+                        {chain.iconUrl && (
+                          <img
+                            alt={chain.name ?? 'Chain icon'}
+                            src={chain.iconUrl}
+                            style={{ width: 12, height: 12 }}
+                          />
+                        )}
+                      </div>
+                    )}
+                    {chain.name}
+                  </button>
+
+                  <button onClick={openAccountModal} type="button">
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ''}
+                  </button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+      {/* <ConnectButton.Custom>
+      {()=> {
+        return (
+          <button onClick={() => connect()}>Connect Wallet</button>
+        )
+      }}
+      </ConnectButton.Custom> */}
     </section>
   );
 };
